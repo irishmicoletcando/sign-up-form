958c362a869113cc7ba5efe29ca23af582eeccdd
Irish Micole Cando <irishmicolecando@gmail.com>
2025-01-04 11:00:25 +0800

    chore: Update configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,21 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'desktop': 'url(/bg-intro-desktop.png)',
+        'mobile': 'url(/bg-intro-mobile.png)',
+      },
+
       colors: {
         red: 'hsl(0, 100%, 74%)',
         green: 'hsl(154, 59%, 51%)',
         blue: 'hsl(248, 32%, 49%)',
         'dark-blue': 'hsl(249, 10%, 26%)',
         'grayish-blue': 'hsl(246, 25%, 77%)',
+      },
+
+      fontFamily: {
+        'poppins': ['Poppins', 'serif']
       }
     },
   },
