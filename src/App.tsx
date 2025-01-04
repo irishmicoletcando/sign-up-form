e82e3adef7a439d98700228d0d461dae875a9833
Irish Micole Cando <irishmicolecando@gmail.com>
2025-01-04 13:32:49 +0800

    fix: Mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import Title from "./components/Title";
 
 export default function App() {
   return (
-    <div className="bg-desktop bg-red h-screen font-poppins text-white p-20">
-      <Title />
-      <FreeTrial />
-      <SignUpForm />
+    <div className="min-h-screen bg-desktop bg-red font-poppins text-white flex items-center">
+      <div className="container mx-auto px-6 lg:px-20">
+        <div className="lg:flex lg:items-center lg:justify-between">
+          <Title />
+          
+          <div className="lg:w-1/2">
+            <FreeTrial />
+            <SignUpForm />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,8 +1,10 @@
 const Title = () => {
   return (
-    <div>
-      <h1 className="font-extrabold text-5xl mb-4">Learn to code by watching others</h1>
-      <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+    <div className="mb-8 lg:mb-0 text-center lg:text-left">
+      <div className="max-w-lg mx-auto lg:mx-0">
+        <h1 className="font-extrabold text-5xl mb-4">Learn to code by watching others</h1>
+        <p className="text-sm md:text-base lg:text-lg opacity-90 leading-relaxed">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+      </div>
     </div>
   )
 }
